3a5130157feed92402ff22483339d61911277590
Javi Leon <javyleon441@gmail.com>
2022-04-09 12:56:27 -0700

    Adding files to Repo

---
+++ app.py
@@ -0,0 +1,90 @@
+from pathlib import Path
+# import fire
+
+#testing changes
+
+import questionary
+
+from questionary.constants import YES
+# import sys
+from qualifier.utils.fileio import load_csv
+
+from qualifier.utils.calculators import (
+    calculate_monthly_debt_ratio,
+    calculate_loan_to_value_ratio,
+)
+from qualifier.utils.filters.credit_score import filter_credit_score
+from qualifier.utils.filters.debit_to_income import filter_debt_to_income
+from qualifier.utils.filters.loan_to_value import filter_loan_to_value
+from qualifier.utils.filters.max_loan import filter_max_loan_size
+from questionary.constants import YES
+def load_bank_data(file_path):
+    csvpath = Path(file_path)
+    return load_csv(csvpath)
+
+
+def find_qualifying_loans(bank_data, credit_score, debt, income, loan, home_value):
+
+    # Calculate the monthly debt ratio
+    monthly_debt_ratio = calculate_monthly_debt_ratio(debt, income)
+    print(f"The monthly debt to income ratio is {monthly_debt_ratio:.02f}")
+
+    # Calculate loan to value ratio
+    loan_to_value_ratio = calculate_loan_to_value_ratio(loan, home_value)
+    print(f"The loan to value ratio is {loan_to_value_ratio:.02f}.")
+
+    # Run qualification filters
+    bank_data_filtered = filter_max_loan_size(loan, bank_data)
+    bank_data_filtered = filter_credit_score(credit_score, bank_data_filtered)
+    bank_data_filtered = filter_debt_to_income(monthly_debt_ratio, bank_data_filtered)
+    bank_data_filtered = filter_loan_to_value(loan_to_value_ratio, bank_data_filtered)
+
+    print(f"Found {len(bank_data_filtered)} qualifying loans")
+
+    return bank_data_filtered
+
+
+# The main function for running the script.
+def run():
+
+    # Load the latest Bank data
+    bank_data = load_bank_data("./data/daily_rate_sheet.csv")
+
+    # Set the applicant's information
+    credit_score = 750
+    debt = 5000
+    income = 20000
+    loan_amount = 100000
+    home_value = 210000
+
+    # Find qualifying loans
+    qualifying_loans = find_qualifying_loans(
+        bank_data, credit_score, debt, income, loan_amount, home_value
+    )
+
+
+def save_qualifying_loans(qualifying_loans):
+    
+    if qualifying_loans:
+    
+        Print_question = questionary.confirm("Would you like to print your qualified loans?").ask()
+
+        if Print_question =="yes" :
+
+            with open(cvspath,"w") as csvfile:
+                csvwriter = csvwriter(csvfile, delimiter=",")
+                csvwriter.writerow(header)
+                for loans in qualifying_loans:
+                    csvwriter.writerow(loans)
+                
+                header = ["Lender,Max Loan Amount","Max LTV,Max DTI,Min Credit Score","Interest Rate"]
+            
+                cvspath = (r"C:\Users\javyl\Desktop\loan_qualifierC2\qualified_loan_list_print")
+        else:
+            print("Thank you")
+    else: 
+        print("Thank you.")
+
+
+if __name__ == "__main__":
+    run()
